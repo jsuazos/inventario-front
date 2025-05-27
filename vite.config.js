a0b404a40ad60cfd8e90161def0f63508a1c75e1
Javier Suazo <javier2587@gmail.com>
2025-05-27 18:43:25 -0400

    corrección config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
-});
+export default {
+  base: './',
+};
