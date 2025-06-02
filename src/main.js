9fb12fa8a2e80e17d279138e0dd29a03bf6b4fd1
Javier Suazo <javier2587@gmail.com>
2025-06-02 19:57:34 -0400

    Actualiza README e ico

--- src/main.js
+++ src/main.js
@@ -54,4 +54,21 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   loadLibrary(libraryData);
+
+  
+  const toggleSidebar = document.getElementById("toggleSidebar");
+  const sidebar = document.getElementById("sidebar");
+
+  toggleSidebar.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+    document.body.classList.toggle("sidebar-open");
+  });
+
+  // Cerrar al hacer clic fuera
+  document.addEventListener("click", (e) => {
+    if (!sidebar.contains(e.target) && !toggleSidebar.contains(e.target)) {
+      sidebar.classList.remove("show");
+      document.body.classList.remove("sidebar-open");
+    }
+  });
 });
