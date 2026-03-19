3b61173311106ac57d6dda18a896c50fd5dee60b
Javier Suazo <javier2587@gmail.com>
2026-03-19 19:01:59 -0300

    mejora filtro

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,16 +4,18 @@ class Filters extends HTMLElement {
         <div class="mb-3 text-end d-lg-block"></div>
             <div class="row mb-3 d-lg-flex">
             <div class="col-md-12">
-                <div class="recibido-filter mt-2">
-                    <input type="radio" name="filterRecibido" id="filterRecibidoTodos" value="" checked>
-                    <label for="filterRecibidoTodos">Todos</label>
+<div class="recibido-filter mt-2">
+  <input type="radio" name="filterRecibido" id="filterRecibidoTodos" value="" checked>
+  <label for="filterRecibidoTodos">Todos</label>
 
-                    <input type="radio" name="filterRecibido" id="filterRecibidoSI" value="SI">
-                    <label for="filterRecibidoSI">Recibidos</label>
+  <input type="radio" name="filterRecibido" id="filterRecibidoSI" value="SI">
+  <label for="filterRecibidoSI">Recibidos</label>
 
-                    <input type="radio" name="filterRecibido" id="filterRecibidoNO" value="NO">
-                    <label for="filterRecibidoNO">No Recibidos</label>
-                </div>
+  <input type="radio" name="filterRecibido" id="filterRecibidoNO" value="NO">
+  <label for="filterRecibidoNO">No Recibidos</label>
+
+  <span class="recibido-filter-slider"></span>
+</div>
             </div>
         </div>
     `;
